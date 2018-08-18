a3031f8dbf74c9e59f43ba1db39f0d9f8cd2fc2c
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2018-08-17 21:45:20 -0400

    Finish implementing CRS

--- crs.py
+++ crs.py
@@ -1,4 +1,6 @@
-code_map = {
+from decimal import Decimal
+
+charge_code_map = {
     "GUILTY": "GTR",
     "GUILTY BY COURT": "GTR",
     "GUILTY - NEGOTIATED/VOLUN PLEA": "GPL",
@@ -15,45 +17,67 @@ code_map = {
     "NOT FILED": "NOTF"
 }
 
-def get_disposition_code(dispositions):
-    if len(dispositions) == 0:
-        return "NOTF"
+def get_primary_charge(charges):
+    if len(charges) == 0:
+        return None
 
-    code = "OTH"
+    charge = charges[0]
+    charge['code'] = "OTH"
     date = None
-    for x in dispositions:
+    for c in charges:
         # NOTE - 01311  SRCR036699 has a withdrawn charge ealier than conviction
         if date is None:
-            date = x[1]
-        elif date != x[1]:
+            date = c['dispositionDate']
+        elif date != c['dispositionDate']:
             continue
 
-        disposition = x[0].replace("DNU-", "")
-        if disposition and disposition not in code_map:
-            print disposition
-        if code == "GTR" or code == "GPL":
+        disposition = c['disposition'].replace("DNU-", "")
+        #if disposition and disposition not in charge_code_map:
+        #    print disposition
+        if charge['code'] == "GTR" or charge['code'] == "GPL":
             continue
+        charge = c
         if not disposition:
-            code = "NOTF"
-        elif disposition not in code_map:
-            code = "OTH"
+            charge['code'] = "NOTF"
+        elif disposition not in charge_code_map:
+            charge['code'] = "OTH"
         else:
-            code = code_map[disposition]
-    return code
+            charge['code'] = charge_code_map[disposition]
+    return charge
 
 def get_finance_column(detail):
     if "FINE" in detail:
         return "J" # FINE
+    if "DEFERRED JUDGMENT CIVIL PENALTY" in detail:
+        return "J" # FINE
+    if "INFRACTIONS-PENALTIES AND FORFEITURES-CITY" in detail:
+        return "J" # FINE
+    if "NONSCHEDULED CHAPTER 321" in detail:
+        return "J" # FINE
+    if "SCHEDULED VIOLATION/NON-SCHEDULED" in detail:
+        return "J" # FINE
+    
     if "FILING" in detail:
         return "K" # FILING
+    if "COURT COSTS" in detail:
+        return "K" # FILING
+    if "TRAFFIC/SIMP MISD APPEAL FEES" in detail:
+        return "K" # FILING
+    if "OTHER SIMPLE MISDEMEANORS" in detail:
+        return "K" # FILING
+
     if "INDIGENT DEFENSE" in detail:
         return "L" # INDIGENT DEFENSE
+
     if "SURCHARGE" in detail:
         return "M" # SURCHARGE
+
     if "RESTITUTION" in detail:
         return "O" # RESTITUTION
+
     if "THIRD PARTY" in detail:
         return "P" # THIRD PARTY
+
     if "SHERIFF" in detail:
         return "Q" # SHERIFF
 
@@ -61,4 +85,39 @@ def get_finance_column(detail):
         return "S" # UNKNOWN
     if "DELINQUENT REVOLVING FUND" in detail:
         return "S" # UNKNOWN
-    return None
+
+    return "R" # MISC
+
+def process_financials(case, worksheet, row):
+    financials = {}
+    col = None
+    for f in case['financials']:
+        if f['amount'] is None:
+            f['amount'] = '0'
+        if not f['detail'].strip():
+            financials[col] -= Decimal(f['paid'])
+            continue
+        col = get_finance_column(f['detail'])
+        if col not in financials:
+            financials[col] = Decimal(0)
+        financials[col] += Decimal(f['amount'])
+        financials[col] -= Decimal(f['paid'])
+
+    for f in financials:
+        worksheet[f + row] = financials[f]
+
+def process_case(case, worksheet, row):
+    i = str(row)
+    worksheet['A' + i] = case['id']
+    worksheet['B' + i] = case['county']
+
+    charge = get_primary_charge(case['charges'])
+    if charge is None:
+        return
+    
+    worksheet['C' + i] = charge['offenseDate']
+    worksheet['D' + i] = charge['dispositionDate']
+    worksheet['E' + i] = charge['description']
+    worksheet['G' + i] = charge['code']
+
+    process_financials(case, worksheet, i)
--- test.py
+++ test.py
@@ -1,73 +1,10 @@
 from bs4 import BeautifulSoup
+from decimal import Decimal
 from openpyxl import load_workbook
 from os import listdir
 from pprint import pprint
+import crs
 import parser
-
-code_map = {
-    "GUILTY": "GTR",
-    "GUILTY BY COURT": "GTR",
-    "GUILTY - NEGOTIATED/VOLUN PLEA": "GPL",
-    "CONVERT TO SIMPLE MISDEM": "GPL",
-    "ACQUITTED": "ACQ",
-    "DISMISSED": "DISM",
-    "DISMISSED BY COURT": "DISM",
-    "DISMISSED BY OTHER": "DISM",
-    "DEFERRED": "DEF",
-    "NOT GUILTY": "ACQ",
-    "WAIVED TO ADULT COURT": "JWV",
-    "ADJUDICATED": "JUV",
-    "WITHDRAWN": "WTHD",
-    "NOT FILED": "NOTF"
-}
-
-def get_disposition_code(dispositions):
-    if len(dispositions) == 0:
-        return "NOTF"
-
-    code = "OTH"
-    date = None
-    for x in dispositions:
-        # NOTE - 01311  SRCR036699 has a withdrawn charge ealier than conviction
-        if date is None:
-            date = x[1]
-        elif date != x[1]:
-            continue
-
-        disposition = x[0].replace("DNU-", "")
-        if disposition and disposition not in code_map:
-            print disposition
-        if code == "GTR" or code == "GPL":
-            continue
-        if not disposition:
-            code = "NOTF"
-        elif disposition not in code_map:
-            code = "OTH"
-        else:
-            code = code_map[disposition]
-    return code
-
-def get_finance_column(detail):
-    if "FINE" in detail:
-        return "J" # FINE
-    if "FILING" in detail:
-        return "K" # FILING
-    if "INDIGENT DEFENSE" in detail:
-        return "L" # INDIGENT DEFENSE
-    if "SURCHARGE" in detail:
-        return "M" # SURCHARGE
-    if "RESTITUTION" in detail:
-        return "O" # RESTITUTION
-    if "THIRD PARTY" in detail:
-        return "P" # THIRD PARTY
-    if "SHERIFF" in detail:
-        return "Q" # SHERIFF
-
-    if "COLLECTION BY CO ATTY" in detail:
-        return "S" # UNKNOWN
-    if "DELINQUENT REVOLVING FUND" in detail:
-        return "S" # UNKNOWN
-    return None
 
 wb = load_workbook('CRS 2.3.2.xlsx')
 ws = wb['CASE DATA']
@@ -80,8 +17,7 @@ for f in files:
 case_ids = list(set(case_ids))
 print len(case_ids)
 
-unknowns = []
-cur_row = 4
+row = 4
 for case_id in case_ids:
     case = {'id': case_id}
     #print "Collecting " + case['id']
@@ -95,23 +31,8 @@ for case_id in case_ids:
     with open("cases/" + case['id'] + "_financials.html", "r") as text_file:
         parser.parse_case_financials(text_file.read(), case)
 
-    dispositions = [(c['disposition'], c['dispositionDate']) for c in case['charges']]
-    #print case['id']
-    #print dispositions
-    #get_disposition_code(dispositions)
+    crs.process_case(case, ws, row)
+    row += 1
 
-    for f in case['financials']:
-        if not f['detail'].strip():
-            continue
-        col = get_finance_column(f['detail'])
-        if col is None:
-            unknowns.append(f['detail'])
-
-    i = str(cur_row)
-    ws['A' + i] = case['id']
-    ws['B' + i] = case['county']
-    cur_row += 1
-
-pprint(set(unknowns))
 wb.save("CRS 2.3.2_test.xlsx")
         
