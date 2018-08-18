694058e285843bc02bd3285f6d77c8db50ea8e90
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2018-08-18 08:43:40 -0400

    Add first html page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hello World"
+	return render_template('start.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    print request.form
+    return render_template('start.html')
 
 if __name__ == "__main__":
 	app.run()
