cf12470887f3e9f3fcd645a2060c7be55eca6e30
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2018-08-19 14:18:39 -0400

    Implement more CRS in website

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from openpyxl import load_workbook
+from pprint import pprint
 from reader import Reader
 import ast
+import crs
 import os
 import parser
 
@@ -58,8 +61,45 @@ def search():
 
 @app.route('/crs', methods=['POST'])
 def get_crs():
+    #if request.form['password'] != os.environ['password']:
+    #    return "ERROR"
+
+    #reader = get_reader()
+    #if reader is None:
+    #    return "ERROR"
+
     data = request.form.getlist('caseIds')
     case_ids = [case_id for ids in data for case_id in ast.literal_eval(ids)]
+    
+    wb = load_workbook('CRS 2.3.2.xlsx')
+    ws = wb['CASE DATA']
+    row = 4
+
+    for case_id in case_ids:
+        case = {'id': case_id}
+        print "Collecting " + case['id']
+        #result = reader.case_summary(case['id'])
+        #parser.parse_case_summary(result, case)
+
+        #result = reader.case_charges()
+        #parser.parse_case_charges(result, case)
+
+        #result = reader.case_financials()
+        #parser.parse_case_financials(result, case)
+
+        with open("cases/" + case['id'] + "_summary.html", "r") as text_file:
+            parser.parse_case_summary(text_file.read(), case)
+
+        with open("cases/" + case['id'] + "_charges.html", "r") as text_file:
+            parser.parse_case_charges(text_file.read(), case)
+
+        with open("cases/" + case['id'] + "_financials.html", "r") as text_file:
+            parser.parse_case_financials(text_file.read(), case)
+        
+        crs.process_case(case, ws, row)
+        row += 1
+
+    wb.save("CRS 2.3.2_test.xlsx")
     return "OK"
 
 if __name__ == "__main__":
