ebc875cb381aaa8479741384d7e296e49ff23db0
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2018-08-20 08:44:20 -0400

    It works now... basically

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, session
+from opener import Opener
 from openpyxl import load_workbook
 from pprint import pprint
 from reader import Reader
@@ -8,6 +9,7 @@ import os
 import parser
 
 app = Flask(__name__)
+app.secret_key = "NOTASECRET"
 
 def get_reader():
     reader = Reader(Opener())
@@ -23,23 +25,23 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    #if request.form['password'] != os.environ['password']:
-    #    return "ERROR"
+    if request.form['password'] != os.environ['password']:
+        return "ERROR"
 
-    #reader = get_reader()
-    #if reader is None:
-    #    return "ERROR"
+    reader = get_reader()
+    if reader is None:
+        return "ERROR"
 
-    first_name = request.form['firstname']
-    last_name = request.form['lastname']
+    session['firstname'] = request.form['firstname']
+    session['lastname'] = request.form['lastname']
 
-    print "Searching ", first_name, last_name
-    #result = reader.search(first_name, last_name)
-    #reader.logoff()
+    print "Searching ", session['firstname'], session['lastname']
+    result = reader.search(session['firstname'], session['lastname'])
+    reader.logoff()
 
-    result = None
-    with open("search_results.html", "r") as text_file:
-        result = text_file.read()
+    #result = None
+    #with open("search_results.html", "r") as text_file:
+    #    result = text_file.read()
 
     print "Parsing results"
     cases = parser.parse_search(result)
@@ -61,12 +63,15 @@ def search():
 
 @app.route('/crs', methods=['POST'])
 def get_crs():
-    #if request.form['password'] != os.environ['password']:
-    #    return "ERROR"
+    if 'firstname' not in session:
+        return "ERROR"
 
-    #reader = get_reader()
-    #if reader is None:
-    #    return "ERROR"
+    reader = get_reader()
+    if reader is None:
+        return "ERROR"
+
+    print "Searching ", session['firstname'], session['lastname']
+    result = reader.search(session['firstname'], session['lastname'])
 
     data = request.form.getlist('caseIds')
     case_ids = [case_id for ids in data for case_id in ast.literal_eval(ids)]
@@ -78,29 +83,31 @@ def get_crs():
     for case_id in case_ids:
         case = {'id': case_id}
         print "Collecting " + case['id']
-        #result = reader.case_summary(case['id'])
-        #parser.parse_case_summary(result, case)
+        result = reader.case_summary(case['id'])
+        parser.parse_case_summary(result, case)
 
-        #result = reader.case_charges()
-        #parser.parse_case_charges(result, case)
+        result = reader.case_charges()
+        parser.parse_case_charges(result, case)
 
-        #result = reader.case_financials()
-        #parser.parse_case_financials(result, case)
+        result = reader.case_financials()
+        parser.parse_case_financials(result, case)
 
-        with open("cases/" + case['id'] + "_summary.html", "r") as text_file:
-            parser.parse_case_summary(text_file.read(), case)
+        #with open("cases/" + case['id'] + "_summary.html", "r") as text_file:
+        #    parser.parse_case_summary(text_file.read(), case)
 
-        with open("cases/" + case['id'] + "_charges.html", "r") as text_file:
-            parser.parse_case_charges(text_file.read(), case)
+        #with open("cases/" + case['id'] + "_charges.html", "r") as text_file:
+        #    parser.parse_case_charges(text_file.read(), case)
 
-        with open("cases/" + case['id'] + "_financials.html", "r") as text_file:
-            parser.parse_case_financials(text_file.read(), case)
+        #with open("cases/" + case['id'] + "_financials.html", "r") as text_file:
+        #    parser.parse_case_financials(text_file.read(), case)
         
         crs.process_case(case, ws, row)
         row += 1
 
-    wb.save("CRS 2.3.2_test.xlsx")
-    return "OK"
+    reader.logoff()
+    
+    wb.save("/tmp/CRS 2.3.2_test.xlsx")
+    return send_file("/tmp/CRS 2.3.2_test.xlsx")
 
 if __name__ == "__main__":
 	app.run()
