e539c45cd44896e740b88c1b35ea0cf401af361a
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2020-03-01 15:47:42 -0500

    Update parser to handle new layout

--- case_parser.py
+++ case_parser.py
@@ -13,14 +13,14 @@ def parse_search(html):
     cases = []
     for row in soup.find_all('tr'):
         cols = row.find_all('td')
-        if len(cols) != 5:
+        if len(cols) != 6:
             continue
         case = {
             'id': list(cols[0].stripped_strings)[0].replace(u'\xa0', u' '),
-            'title': cols[1].string,
-            'name': cols[2].string.strip(),
-            'dob': cols[3].string.replace(u'\xa0', u''),
-            'role': cols[4].string
+            'title': cols[2].string,
+            'name': cols[3].string.strip(),
+            'dob': cols[4].string.replace(u'\xa0', u''),
+            'role': cols[5].string
         }
         if case['id'] == 'Case ID':
             continue
@@ -34,7 +34,7 @@ def parse_case_summary(html, case):
     with open(tmp_dir + case['id'] + "_summary.html", "w") as text_file:
         text_file.write(html)
     soup = BeautifulSoup(html, 'html.parser')
-    case['county'] = soup.find_all('tr')[2].find_all('td')[1].string
+    case['county'] = soup.find_all('tr')[2].find_all('td')[0].string
 
 def parse_case_charges(html, case):
     with open(tmp_dir + case['id'] + "_charges.html", "w") as text_file:
@@ -45,7 +45,7 @@ def parse_case_charges(html, case):
     cur_section = None
     rows = soup.find_all('tr')
     for row in rows:
-        cols = row.find_all('td')
+        cols = row.find_all('font')
         texts = [
             ''.join(col.find_all(text=True))
                 .replace(u'\xa0', u' ')
@@ -62,25 +62,23 @@ def parse_case_charges(html, case):
             if cur_charge is not None:
                 charges.append(cur_charge)
             cur_charge = {}
+            cur_section = "Charge"
 
-        if len(texts) == 2:
-            if texts[1] == "Charge":
-                cur_section = "Charge"
-            if texts[1] == "Adjudication":
-                cur_section = "Adjudication"
-            if texts[1] == "Sentence":
-                cur_section = "Sentence"
+        if texts[0] == "Adjudication":
+            cur_section = "Adjudication"
+        if texts[0] == "Sentence":
+            cur_section = "Sentence"
         
         if cur_section == "Charge":
-            if len(texts) >= 5 and texts[3].startswith("Description:"):
-                cur_charge['description'] = texts[4]
-            if len(texts) >= 3 and texts[1].startswith("Offense Date:"):
-                cur_charge['offenseDate'] = texts[2]
+            if len(texts) >= 4 and texts[2].startswith("Description:"):
+                cur_charge['description'] = texts[3]
+            if len(texts) >= 2 and texts[0].startswith("Offense Date:"):
+                cur_charge['offenseDate'] = texts[1]
         
         if cur_section == "Adjudication":
-            if len(texts) >= 5 and texts[1].startswith("Adj.:"):
-                cur_charge['disposition'] = texts[2]
-                cur_charge['dispositionDate'] = texts[4]
+            if len(texts) >= 4 and texts[0].startswith("Adjudication:"):
+                cur_charge['disposition'] = texts[1]
+                cur_charge['dispositionDate'] = texts[3]
 
     if cur_charge is not None:
         charges.append(cur_charge)
