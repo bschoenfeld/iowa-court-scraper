6c2250db89f55573a105c8412d72ea2516544fad
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2020-04-18 11:32:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,18 @@ from openpyxl import load_workbook
 from os import listdir
 from pprint import pprint
 import crs
-import parser
+import case_parser
+import platform
+
+tmp_dir = '/tmp/'
+if platform.system() == 'Windows':
+    tmp_dir = '.\\tmp\\'
 
 wb = load_workbook('CRS 2.3.2.xlsx')
 ws = wb['CASE DATA']
 
 case_ids = []
-files = listdir("./cases/")
+files = listdir(tmp_dir)
 for f in files:
     if f.endswith(".html") and f != "search_results.html":
         case_ids.append(f.split("_")[0])
@@ -22,17 +27,19 @@ for case_id in case_ids:
     case = {'id': case_id}
     #print "Collecting " + case['id']
 
-    with open("cases/" + case['id'] + "_summary.html", "r") as text_file:
-        parser.parse_case_summary(text_file.read(), case)
+    with open(tmp_dir + case['id'] + "_summary.html", "r") as text_file:
+        case_parser.parse_case_summary(text_file.read(), case)
 
-    with open("cases/" + case['id'] + "_charges.html", "r") as text_file:
-        parser.parse_case_charges(text_file.read(), case)
+    with open(tmp_dir + case['id'] + "_charges.html", "r") as text_file:
+        case_parser.parse_case_charges(text_file.read(), case)
 
-    with open("cases/" + case['id'] + "_financials.html", "r") as text_file:
-        parser.parse_case_financials(text_file.read(), case)
+    with open(tmp_dir + case['id'] + "_financials.html", "r") as text_file:
+        case_parser.parse_case_financials(text_file.read(), case)
 
-    crs.process_case(case, ws, row)
+    print "===", row - 3, "of", len(case_ids), "==="
+    pprint(case)
+    #crs.process_case(case, ws, row)
     row += 1
 
-wb.save("CRS 2.3.2_test.xlsx")
+#wb.save("CRS 2.3.2_test.xlsx")
         
